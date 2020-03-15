f57a8ef247f6c3b408a5b76d0f78e854f1831984
Brandon Cate <brandoncate95@gmail.com>
2020-03-15 12:08:41 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let name;
+	let count = 0;
+
+	function handleClick(){
+		count += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}! Nice to meet you!</h1>
+<p>
+	You got a problem?
+</p>
+
+<button on:click={handleClick}>
+Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
